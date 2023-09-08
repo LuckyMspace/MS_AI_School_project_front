42b35a63be7e6d2cfd0b382dbb262ec5f1bd6bb9
LuckyMspace <caps813@gmail.com>
2023-09-08 11:01:42 +0900

    updated some files

---
+++ result_module.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def result():
+    st.session_state['result']  # p_5
+    st.text("일단 여기 세션으로 이동 성공")
+    # 어떤 방식으로 보여줄지 고민해봐야함.
+    # 이미지 개수가 다르게 들어와도 일괄적으로 보여줄 수 있어야함.
+    # url로 이미지 보여주는 기능 됨
+    
