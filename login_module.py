1ec59e137cf7e7e0cade1047ee2f9165bb7d4928
acensia <acensia@snu.ac.kr>
2023-09-11 16:08:12 +0900

    login logic

--- login_module.py
+++ login_module.py
@@ -14,8 +14,7 @@ def login_section():
         left_column.markdown("<h1 style='font-size: 36px;'>AI패션 추천 서비스</h1>", unsafe_allow_html=True)
         email = left_column.text_input("이메일 주소", key='login_email')
         password = left_column.text_input("비밀번호", type="password", key='login_password')
-
-        if left_column.button("로그인"):
+        def onLogin():
             try:
                 response = requests.post("http://localhost:5000/login", data={"email": email, "password": password})
                 if response.status_code == 200:
@@ -30,9 +29,10 @@ def login_section():
                     left_column.error("로그인 실패")
             except Exception as e:
                 st.error(f"서버와 통신 중 문제가 발생했습니다: {e}")
-
-        elif left_column.button("회원가입"):
+        def onSignUp():
             st.session_state['sign_up'] = True
+        left_column.button("로그인", on_click=onLogin)
+        left_column.button("회원가입", on_click=onSignUp)
 
     right_column.image("./front_images/main_image.jpg", use_column_width=True)
 
